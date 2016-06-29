0274686ab379acc8c2336b75af55c42b941d4d7e
Gerard Moreno-Torres Bertran <germtf@gmail.com>
2016-06-29 15:29:17 +0100

    First implementation of chagi

--- src/index.js
+++ src/index.js
@@ -2,7 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../style/style.css';
 
+class ChagiList extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      chagis: []
+    };
+    setInterval(() => this.update(), 100);
+  }
+
+  update() {
+    this.setState({
+      chagis: [{x: Math.random() * 1000, y: Math.random() * 1000}, ...this.state.chagis]
+    });
+  }
+
+  render() {
+    const chagis = this.state.chagis.map(c => (
+      <p key={c.x * c.y} style={{
+        left: c.x,
+        top: c.y,
+        position: 'absolute'
+      }}> 내자기 </p>
+    ));
+    return (
+      <div> {chagis} </div>
+    );
+  }
+};
+
+
 ReactDOM.render(
-  (<p> Hello react </p>),
+  (<div>
+    <ChagiList/>
+  </div>),
   document.getElementById('app')
 );
+
